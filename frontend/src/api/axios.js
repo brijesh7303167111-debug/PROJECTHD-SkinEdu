90efd8d6c6edbd190b181367c937e0ed129b11e4
brijesh7303167111-debug <brijesh7303167111@gmail.com>
2025-10-30 14:01:42 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://project-hd.onrender.com",
+  baseURL: "https://projecthd-skinedu.onrender.com",
+ 
   // baseURL: "http://localhost:3000",
 });
 
