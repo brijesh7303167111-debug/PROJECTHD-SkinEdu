1534248ccc373e8b724e171bcadba32aaada0b12
brijesh kumar <brijesh6514@gmail.com>
2025-10-30 23:17:37 +0530

    otp

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://projecthd-skinedu.onrender.com/",
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://projecthd-skinedu.onrender.com/",
+  // baseURL: "http://localhost:3000/",
 });
 
 
